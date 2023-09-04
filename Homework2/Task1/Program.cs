98354b3a5a6783bf8e5a9ef09fc0b0d8efaeb95f
Sheina Evgueniya <95032717+JennieGB@users.noreply.github.com>
2023-09-04 15:46:23 +0300

    Add files via upload

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 1: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+123456789 -> 3
+*/
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+if (numberArray.Length >= 3)
+{
+    Console.Write($"В числе {number} третья цифра - это {numberArray[2]}");
+}
+else
+{
+    Console.Write($"В числе {number} третья цифра отсутствует");
+};
---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+if (weekday == 6 || weekday == 7)
+{
+    Console.WriteLine($"{weekday} Да, это выходной день")
+}
+else if (weekday >= 1 && weekday <= 5)
+{
+    Console.WriteLine($"{weekday} Нет, это будни")
+}
+else
+{
+Console.WriteLine($"{weekday} такого дня недели не существует")
+};
