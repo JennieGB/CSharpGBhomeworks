c32f8b49e003885d38a65074d152b07fe0f28364
Sheina Evgueniya <95032717+JennieGB@users.noreply.github.com>
2023-09-18 17:33:56 +0300

    Add files via upload

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3  
+*/
+Console.Write("Введите число, которое будет обозначать количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrNumbers = new int[m];
+void InputNumbers(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        arrNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int Compare(int[] arrNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < arrNumbers.Length; i++)
+    {
+        if (arrNumbers[i] > 0) count += 1;
+    }
+    return count;
+}
+InputNumbers(m);
+Console.WriteLine($"Введено чисел больше 0: {Compare(arrNumbers)} ");
+    
