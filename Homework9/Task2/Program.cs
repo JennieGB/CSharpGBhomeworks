4ebafcf8b9a26a9976705681d7e0a1907dcf55b8
Sheina Evgueniya <95032717+JennieGB@users.noreply.github.com>
2023-09-28 16:14:27 +0300

    Add files via upload

---
+++ Homework9/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 2. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите число, обозначающее начало промежутка чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, обозначающее конец промежутка чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int count = m; count <= n; count++) { sum = sum + count; }
+Console.Write($"{sum}");
+Console.WriteLine();
